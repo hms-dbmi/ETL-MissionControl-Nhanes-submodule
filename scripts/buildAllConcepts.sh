11d8ebb013bbde97b322be2e8539d15b3fe2ae8a
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2020-02-12 12:19:12 -0500

    initial build all concepts

---
+++ scripts/buildAllConcepts.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+###
+# This scripts will run the necassary steps to gather the requried files from s3
+# and execute the generate the allConcepts.csv needed to generate javabin files for HPDS
+#
+
+print "Pulling nhanes data and configuration from s3"
+
+aws s3 cp s3://avillach-73-nhanes-etl/data/ ./data/ --recursive
+aws s3 cp s3://avillach-73-nhanes-etl/mappings/ ./mappings/ --recursive
+aws s3 cp s3://avillach-73-nhanes-etl/resources/ ./resources/ --recursive
+
+print "Generating allConcepts.csv"
+
+java -jar GenerateAllConcepts.csv
+
+print "Finished generating allConcepts.csv"
